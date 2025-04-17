efaa9770e5b345aa566862971a1a7d87549c0f59
mjkats <michelkatsilis@gmail.com>
2025-04-16 22:06:38 -0300

    Creating project skeleton

---
+++ src/test/java/br/com/katsilis/mercadolance/MercadoLanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.katsilis.mercadolance.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
